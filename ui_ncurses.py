6dc6ae3319b6843d3d4ed781653fc97e0017d189
Andreas Zwinkau <beza1e1@web.de>
2011-04-27 18:35:22 +0200

    curses: select different addresses

--- ui_ncurses.py
+++ ui_ncurses.py
@@ -6,7 +6,7 @@ import ncards
 
 ENTER = ord("\n")
 
-def draw(screen, string, cards):
+def draw(screen, string, cards, pos):
    my, mx = screen.getmaxyx()
    for i in range(0,my-2):
       if i < len(cards):
@@ -15,11 +15,18 @@ def draw(screen, string, cards):
          if not name:
             name = card.get("email", "?")
          line = name.encode("utf8")
-         screen.addstr(i+1, 1, line+"\n")
+         if i+1 == pos:
+            screen.addstr(i+1, 1, line+"\n", curses.A_REVERSE)
+         else:
+            screen.addstr(i+1, 1, line+"\n")
       else:
          screen.addstr("\n")
    screen.addstr(0, 0, "\n") # clear line
    screen.addstr(0, 0, "> "+string.encode("utf8"))
+   if pos == 0:
+      curses.curs_set(2)
+   else:
+      curses.curs_set(0)
    screen.refresh()
 
 def search(string, cards):
@@ -29,10 +36,12 @@ def search(string, cards):
    return res
 
 def app(screen, all_cards):
+   my, mx = screen.getmaxyx()
    encoding = "utf8"
    string = ""
    cards = []
-   draw(screen, string, cards)
+   selected = 0 # selected row on screen
+   draw(screen, string, cards, selected)
    try:
       while True:
          ch = screen.getch()
@@ -40,22 +49,27 @@ def app(screen, all_cards):
             break
          elif ch == curses.KEY_BACKSPACE:
             string = string.decode(encoding)[:-1].encode(encoding)
+         elif ch == curses.KEY_DOWN:
+            selected = min(my-2, selected+1)
+         elif ch == curses.KEY_UP:
+            selected = max(0, selected-1)
          elif ch == ENTER:
-            pass # TODO
-         elif ch < 128:
-            string += chr(ch)
-         elif ch < 256:
+            selected = 1
+         elif selected == 0: # search string entering
+            assert 13 < ch < 256
             string += chr(ch)
             try:
                string.decode(encoding)
             except UnicodeDecodeError:
                # read one more char
                continue
+         elif chr(ch) in "sS":
+            selected = 0
          else:
             pass # TODO
          ustring = string.decode("utf8")[:20]
          cards = search(ustring, all_cards)
-         draw(screen, ustring, cards)
+         draw(screen, ustring, cards, selected)
    except KeyboardInterrupt:
       pass
 
